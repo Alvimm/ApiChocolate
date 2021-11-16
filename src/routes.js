e08442afd3c4bc6234c79ff807d6da99c82cf8e1
Alvimm <filipe2012alvim@gmail.com>
2021-11-16 03:32:09 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -2,24 +2,23 @@ const routes = require('express').Router();
 const multer = require('multer');
 const userController = require('./app/controllers/userController');
 const chocolateController = require('./app/controllers/chocolateController');
-const jwt = require('./app/middlewares/jwt');
+// const jwt = require('./app/middlewares/jwt');
 const multerConfig = require('./config/multer');
-const validators = require('./app/middlewares/validators');
+// const validators = require('./app/middlewares/validators');
 
-
+routes.post('/users', userController.store);
 routes.post('/login', userController.auth);
 
-routes.use(jwt);
+// routes.use(jwt);
 
 routes.get('/users', userController.index);
-routes.post('/users', validators.userCreateValidator, userController.store);
-routes.put('/users/:id', validators.userUpdateValidator, userController.update);
+routes.put('/users/:id', userController.update);
 routes.delete('/users/:id', userController.delete);
 routes.get('/users/:id', userController.show);
 
 routes.get('/chocolates', chocolateController.index);
-routes.post('/chocolates', validators.chocolateValidator, multer(multerConfig).single('file'), chocolateController.store);
-routes.put('/chocolates/:id', validators.chocolateValidator, chocolateController.update);
+routes.post('/chocolates', multer(multerConfig).single('file'), chocolateController.store);
+routes.put('/chocolates/:id', chocolateController.update);
 routes.delete('/chocolates/:id', chocolateController.delete);
 routes.get('/chocolates/:id', chocolateController.show);
 
